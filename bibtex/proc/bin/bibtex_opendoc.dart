de4009d359df2c3913136939ef20fb5a9a04e2ce
halka9000stg <HALKA9000-NIIP@github.com>
2022-06-15 07:52:18 +0900

    add: ディレクトリ基礎及び暫定HP

---
+++ bibtex/proc/bin/bibtex_opendoc.dart
@@ -0,0 +1,5 @@
+import 'package:bibtex_opendoc/bibtex_opendoc.dart' as bibtex_opendoc;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bibtex_opendoc.calculate()}!');
+}
---
+++ bibtex/proc/test/bibtex_opendoc_test.dart
@@ -0,0 +1,8 @@
+import 'package:bibtex_opendoc/bibtex_opendoc.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
